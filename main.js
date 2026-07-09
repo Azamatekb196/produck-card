5288c5a954bce92c6d15f257c616bd3d79690414
Azamatekb196 <region.ekb96@gmail.com>
2026-07-09 19:03:15 +0500

    corrected errors

--- main.js
+++ main.js
@@ -1,42 +1,41 @@
 
 
 const productCard = document.querySelector('.card');
-const changeColorCardButton = document.querySelector('#change-color-card-button')
-const colorHashGreen = 'rgb(208, 255, 0)'
+const changeColorCardButton = document.querySelector('#change-color-card-button');
+const colorHashGreen = 'rgb(208, 255, 0)';
 
 changeColorCardButton.addEventListener('click', () => {
   productCard.style.backgroundColor = colorHashGreen;
-})
+});
 
 
-
-const changeColorAllCardsButton = document.querySelector('#change-color-cards-button')
+const changeColorAllCardsButton = document.querySelector('#change-color-cards-button');
 const allProductCards = document.querySelectorAll('.card');
-const colorHashTurquoise = 'hsl(155, 100%, 50%)'
+const colorHashTurquoise = 'hsl(155, 100%, 50%)';
 
 changeColorAllCardsButton.addEventListener('click', () => {
-  allProductCards.forEach((card) => card.style.backgroundColor = colorHashTurquoise)
-})
+  allProductCards.forEach((card) => card.style.backgroundColor = colorHashTurquoise);
+});
 
 
-const openGoogleButton = document.querySelector('#open-google')
-const googleUrl = 'https://google.com'
+const openGoogleButton = document.querySelector('#open-google');
+const googleUrl = 'https://google.com';
 
-openGoogleButton.addEventListener('click', openGoogle)
+openGoogleButton.addEventListener('click', openGoogle);
 
 function openGoogle() {
-  const answer = window.confirm('вы действительно хотите открыть Google')
+  const answer = window.confirm('вы действительно хотите открыть Google');
 
-  if(answer === true) {
-    window.open(googleUrl)
+  if (answer === true) {
+    window.open(googleUrl);
   }
 }
 
-const outputLogButton = document.querySelector('#output-consol-log')
+const outputLogButton = document.querySelector('#output-consol-log');
 
-outputLogButton.addEventListener('click', () => outputConsoleLog('Привет!'))
+outputLogButton.addEventListener('click', () => outputConsoleLog('Привет!'));
 
 function outputConsoleLog(message) {
-  alert(message)
-  console.log(message)
+  alert(message);
+  console.log(message);
 }
