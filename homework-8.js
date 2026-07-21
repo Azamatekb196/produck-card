daeae0abbe032c8914416e268d0cb1d714d9a83a
Azamatekb196 <region.ekb96@gmail.com>
2026-07-21 10:36:44 +0500

    homework8

--- homework-8.js
+++ homework-8.js
@@ -1,3 +1,6 @@
+
+// объект на основе данных
+
 const user = {
   name: 'Vadim',
   surname: 'Vadimovish',
@@ -7,4 +10,33 @@ const user = {
   work: 'developer',
 }
 
-console.log(user)
+// объект который хранит данные об автомобиле и дополнительное свойство - владелец авто
+
+const car = {
+  brend: 'Mercedes',
+  color: 'black',
+  model: 'Benz V-Класс',
+  typeEngine: 'diesel',
+  displacementEngine: '2.1l',
+}
+
+car.owner = user
+
+// console.log(car)
+// console.log(car.owner.name)
+
+// функция которая аргументом  принимает объект, проверяет есть ли объект, если есть ничего не делает, если нет то добавляет
+
+function checkMaxSpeed (car) {
+  if ('maxSpeed' in car) {
+    console.log(car.maxSpeed)
+  } else {
+    car.maxSpeed = 330
+  }
+}
+
+checkMaxSpeed (car)
+console.log(car.maxSpeed)
+
+// функция получает первым аргументом — объект, а вторым аргументом — свойство объекта и выводит его значение
+
