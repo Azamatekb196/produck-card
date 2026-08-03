d5d9984051cee5dfe9aba223dd1798c2210bca74
Azamatekb196 <region.ekb96@gmail.com>
2026-08-03 18:52:13 +0500

    add-09-homework

---
+++ homework-9.js
@@ -0,0 +1,20 @@
+// уровень 1
+
+// задание 2
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function getNumberFromFive (numbers) {
+  return numbers.filter(item => item >= 5)
+}
+const numberFromFive = getNumberFromFive(numbers)
+// console.log(numberFromFive)
+
+
+
+// задание 3
+const warehouse = ['Ноутбук', 'Мышь', 'Телефон', 'Чехол'];
+
+const productExists = warehouse.includes('Телефон')
+// console.log(productExists)
+
+// задание 4
